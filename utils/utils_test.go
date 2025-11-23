1befde926f43958405c5b2c8c605951b5ae8fd21
Jinish Trivedi <jinishtrivedi9302@gmail.com>
2025-11-23 15:43:34 +0530

    #1: Removed test.yaml workflow, removed failed test case

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -5,7 +5,6 @@ import "testing"
 // TestNoUtilFunction tests the correctness of NoUtilFunction by checking if it returns the expected value and error.
 func TestNoUtilFunction(t *testing.T) {
 	value, err := NoUtilFunction()
-	t.Errorf("Expected: 0, <nil> || Got: %v, %v", value, err)
 	if value != 0 || err != nil {
 		t.Errorf("Expected: 0, <nil> || Got: %v, %v", value, err)
 	}
